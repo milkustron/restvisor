a105b52d4bd72aabdd8635a1ea3e4f1888d38b7d
Antonio Hidalgo <antoniohidalgomoraz999@gmail.com>
2025-02-25 19:41:58 +0000

    [mod] menu.js, menu-operation.html menu-table.html

---
+++ src/static/js/menu.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function loadComponent(id, url) {
+        fetch(url)
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById(id).innerHTML = data;
+            })
+            .catch(error => console.error("Error cargando el componente:", error));
+    }
+
+    loadComponent("navbar", "../templates/fragments/navbar.html");
+    loadComponent("data-table", "../templates/fragments/menu-table.html");
+    loadComponent("addNewModal", "../templates/fragments/modals/add_new_modal.html");
+});
