4517aa667ec24012b8900ae98ca5b405b957dd6d
Antonio Hidalgo <antoniohidalgomoraz999@gmail.com>
2025-02-26 14:03:34 +0000

    [fix] editmenu-modal.html

--- src/static/js/menu.js
+++ src/static/js/menu.js
@@ -11,4 +11,5 @@ document.addEventListener("DOMContentLoaded", function () {
     loadComponent("navbarmenu", "../templates/fragments/navbaroperationmenu.html");
     loadComponent("data-table", "../templates/fragments/menu-table.html");
     loadComponent("eliminationModal", "../templates/fragments/modals/elimination-modal.html");
+    loadComponent("editModal", "../templates/fragments/modals/editmenu-modal.html");
 });
