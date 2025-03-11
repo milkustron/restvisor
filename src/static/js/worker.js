0988c160453459f774cb3c20a8690a78ac2e27c7
PedroBatistaS <pedrobatistasosa2002@gmail.com>
2025-03-11 19:43:37 +0000

    [fix] shifts-tables from json

--- src/static/js/worker.js
+++ src/static/js/worker.js
@@ -11,6 +11,43 @@ document.addEventListener("DOMContentLoaded", function () {
     loadComponent("navbarworker", "../templates/fragments/navbarworker.html");
     loadComponent("shifts-table", "../templates/fragments/shifts-table-clock-in.html");
     loadComponent("worker-schedule", "../templates/fragments/worker-schedule.html");
+
+    function loadWorkersData() {
+        fetch('../static/database.json')  
+            .then(response => response.json())
+            .then(data => {
+                console.log("Datos de trabajadores:", data);
+                renderShiftsTable(data);  
+            })
+            .catch(error => console.error("Error loading workers data:", error));
+    }
+
+    
+    loadWorkersData();
+
+    
+    function renderShiftsTable(data) {
+        const table = document.getElementById('shifts-table');
+        const tbody = table.querySelector('tbody');
+        
+        tbody.innerHTML = ''; 
+        const worker = data.workers.find(worker => worker.id === 1);
+
+        if (worker) {
+            worker.worker_hours.forEach(shift => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>${shift.date}</td>
+                    <td>${shift.start}</td>
+                    <td>${shift.finish}</td>
+                    <td>${shift.total_amount}</td>
+                `;
+                tbody.appendChild(row);
+            });
+        } else {
+            console.log("Trabajador no encontrado");
+        }
+    }
 });
 
 function changeText() {
