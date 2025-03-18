9e91c202b7f850a34b13714a12fc688ed8b50e30
unknown <Pedro>
2025-03-18 01:45:59 +0000

    [fix] validation sign-up form

--- src/static/js/sign-up.js
+++ src/static/js/sign-up.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function loadComponent(id, url) {
+    function loadComponent(id, url, callback) {
         fetch(url)
             .then(response => response.text())
             .then(data => {
                 document.getElementById(id).innerHTML = data;
+                if (callback) callback();
             })
             .catch(error => console.error("Error loading component:", error));
     }
@@ -13,27 +14,30 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     loadComponent("navbarworker", "../templates/fragments/navbarworker.html");
-    loadComponent("sign-up-form", "../templates/fragments/sign-up-form.html");
 
-    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{8,}$/;
-    const postalCodeRegex = /^\d{5}$/;
-    
-    const password = document.getElementById("password").value;
-    const zipcode = document.getElementById("zip").value;
+    loadComponent("sign-up-form", "../templates/fragments/sign-up-form.html", function () {
+        
+        const password = document.getElementById("password");
+        const zipcode = document.getElementById("zip");
 
-    password.addEventListener("input", function (evt) {
-        if(!validarTexto(password, passwordRegex)) {
-            password.setCustomValidity("La contraseña debe tener al menos 8 caracteres, incluyendo 1 letra, 1 número y 1 carácter especial");
-        } else {
-            password.setCustomValidity("");
-        }
-    });
+        const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{8,}$/;
+        const postalCodeRegex = /^\d{5}$/;
+        
 
-    zipcode.addEventListener("input", function (evt) {
-        if(!validarTexto(zipcode, postalCodeRegex)) {
-            password.setCustomValidity("El código postal debe tener exactamente 5 dígitos.");
-        } else {
-            password.setCustomValidity("");
-        }
+        password.addEventListener("input", function () {
+            if (!validarTexto(password.value, passwordRegex)) {
+                password.setCustomValidity("La contraseña debe tener al menos 8 caracteres, incluyendo 1 letra, 1 número y 1 carácter especial");
+            } else {
+                password.setCustomValidity("");
+            }
+        });
+
+        zipcode.addEventListener("input", function () {
+            if (!validarTexto(zipcode.value, postalCodeRegex)) {
+                zipcode.setCustomValidity("El código postal debe tener exactamente 5 dígitos.");
+            } else {
+                zipcode.setCustomValidity("");
+            }
+        });
     });
 });
