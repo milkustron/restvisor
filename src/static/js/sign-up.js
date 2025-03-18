d432423948fec6ba52737e3a941faeaa08ea0280
PedroBatistaS <pedrobatistasosa2002@gmail.com>
2025-03-18 01:32:25 +0000

    [add] validation for sign-up form

--- src/static/js/sign-up.js
+++ src/static/js/sign-up.js
@@ -8,8 +8,32 @@ document.addEventListener("DOMContentLoaded", function () {
             .catch(error => console.error("Error loading component:", error));
     }
 
+    function validarTexto(texto, regex) {
+        return regex.test(texto);
+    }
+
     loadComponent("navbarworker", "../templates/fragments/navbarworker.html");
     loadComponent("sign-up-form", "../templates/fragments/sign-up-form.html");
-    loadComponent("shifts-table", "../templates/fragments/shifts-table.html");
-    loadComponent("worker-schedule", "../templates/fragments/worker-schedule.html");
+
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{8,}$/;
+    const postalCodeRegex = /^\d{5}$/;
+    
+    const password = document.getElementById("password").value;
+    const zipcode = document.getElementById("zip").value;
+
+    password.addEventListener("input", function (evt) {
+        if(!validarTexto(password, passwordRegex)) {
+            password.setCustomValidity("La contraseña debe tener al menos 8 caracteres, incluyendo 1 letra, 1 número y 1 carácter especial");
+        } else {
+            password.setCustomValidity("");
+        }
+    });
+
+    zipcode.addEventListener("input", function (evt) {
+        if(!validarTexto(zipcode, postalCodeRegex)) {
+            password.setCustomValidity("El código postal debe tener exactamente 5 dígitos.");
+        } else {
+            password.setCustomValidity("");
+        }
+    });
 });
