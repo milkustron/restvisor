f5591eb6f4248ee81eb88ec8758e904a430029be
hupog <hugogonzalez798@gmail.com>
2025-03-31 16:31:46 +0100

    configure enviroment

---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDHAtQI2VBlhvYI8Vtmy7Pk2LqBUy4O1cM",
+        authDomain: "restvisor-887b9.firebaseapp.com",
+        projectId: "restvisor-887b9",
+        storageBucket: "restvisor-887b9.firebasestorage.app",
+        messagingSenderId: "674854508849",
+        appId: "1:674854508849:web:dd6b8ca735af7f4ab07810",
+        measurementId: "G-Z6GVHFKTXD"
+    }
+};
