71f60a9b07f74aacd4c18c323bc8e5f6930d4f38
Antonio Hidalgo <antoniohidalgomoraz999@gmail.com>
2025-04-07 16:40:12 +0100

    [add] component menu-table

---
+++ src/app/components/menu-table/menu-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-table',
+  imports: [],
+  templateUrl: './menu-table.component.html',
+  styleUrl: './menu-table.component.css'
+})
+export class MenuTableComponent {
+
+}
