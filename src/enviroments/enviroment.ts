d4081e1269f1fd4f44c6f53990cf9376757c87b8
hupog <hugogonzalez798@gmail.com>
2025-04-07 15:50:42 +0100

    [add] firebase connection

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,12 +1,10 @@
-export const environment = {
-    production: false,
-    firebaseConfig: {
-        apiKey: "AIzaSyDHAtQI2VBlhvYI8Vtmy7Pk2LqBUy4O1cM",
-        authDomain: "restvisor-887b9.firebaseapp.com",
-        projectId: "restvisor-887b9",
-        storageBucket: "restvisor-887b9.firebasestorage.app",
-        messagingSenderId: "674854508849",
-        appId: "1:674854508849:web:dd6b8ca735af7f4ab07810",
-        measurementId: "G-Z6GVHFKTXD"
-    }
+export const firebaseConfig = {
+    apiKey: "AIzaSyDHAtQI2VBlhvYI8Vtmy7Pk2LqBUy4O1cM",
+    authDomain: "restvisor-887b9.firebaseapp.com",
+    databaseURL: "https://restvisor-887b9-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "restvisor-887b9",
+    storageBucket: "restvisor-887b9.firebasestorage.app",
+    messagingSenderId: "674854508849",
+    appId: "1:674854508849:web:27551aa77a8e43b7b07810",
+    measurementId: "G-0WNC6K2MJE"
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from './enviroments/enviroment';
+
+// Inicializa Firebase antes de arrancar la app
+initializeApp(firebaseConfig);
 
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+    .catch(err => console.error(err));
