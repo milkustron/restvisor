6bde2de493db2388a32c13bd93fa624f43656cd7
hupog <hugogonzalez798@gmail.com>
2025-04-07 16:46:41 +0100

    [wip] index

--- src/app/shared/navbar-main/navbar-main.component.ts
+++ src/app/shared/navbar-main/navbar-main.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-main',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgOptimizedImage],
   templateUrl: './navbar-main.component.html',
   styleUrls: ['./navbar-main.component.css']
 })
