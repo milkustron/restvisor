f5567edc3fa9836dafe80ee5768ef0a45894c18c
PedroBatistaS <pedrobatistasosa2002@gmail.com>
2025-04-08 19:58:29 +0100

    [fix] data in shifts-table

--- src/app/shared/shifts-table/shifts-table.component.ts
+++ src/app/shared/shifts-table/shifts-table.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './shifts-table.component.css'
 })
 export class ShiftsTableComponent {
-
+  shifts = [
+    { date: '2025-04-01', start: '09:00', end: '17:00', total: '8h' },
+    { date: '2025-04-02', start: '10:00', end: '18:00', total: '8h' },
+    { date: '2025-04-03', start: '08:30', end: '16:30', total: '8h' },
+  ];
 }
