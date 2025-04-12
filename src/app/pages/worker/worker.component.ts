1fa29d84c9575a8c180d76fd5a8fe2b68dc261a7
PedroBatistaS <pedrobatistasosa2002@gmail.com>
2025-04-12 10:57:12 +0100

    [add] worker components to worker page

--- src/app/pages/worker/worker.component.ts
+++ src/app/pages/worker/worker.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ShiftsTableComponent } from '../../shared/shifts-table/shifts-table.component'
+import { WorkerScheduleComponent } from '../../shared/worker-schedule/worker-schedule.component';
 
 @Component({
   selector: 'app-worker',
-  imports: [ShiftsTableComponent],
+  imports: [ShiftsTableComponent, WorkerScheduleComponent],
   templateUrl: './worker.component.html',
   styleUrl: './worker.component.css'
 })
--- src/app/shared/worker-schedule/worker-schedule.component.ts
+++ src/app/shared/worker-schedule/worker-schedule.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-worker-schedule',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './worker-schedule.component.html',
-  styleUrl: './worker-schedule.component.css'
+  styleUrls: ['./worker-schedule.component.css']
 })
 export class WorkerScheduleComponent {
-
+  schedule = [
+    { day: 'Monday', start: '09:00', finish: '17:00' },
+    { day: 'Tuesday', start: '10:00', finish: '18:00' },
+    { day: 'Wednesday', start: '08:30', finish: '16:30' }
+  ];
 }
