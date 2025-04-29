0e491c5b014fb1b8258d33440896b00a1e0e543c
PedroBatistaS <pedrobatistasosa2002@gmail.com>
2025-04-29 19:39:57 +0100

    [add] shifts-models

---
+++ src/app/models/shift-model.ts
@@ -0,0 +1,10 @@
+import { Timestamp } from "firebase/firestore";
+
+export interface shift {
+    date: Timestamp, 
+    start: Timestamp,
+    end: Timestamp,
+    total: string, 
+    state: 'ongoing' | 'finished',
+    employeeId: string
+}
