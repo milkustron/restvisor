2db1adc6c5994cf05c7c418a1d3ed0dd3f42e2a9
Pedro <pedrobatistasosa2002@gmail.com>
2025-05-02 18:46:33 +0100

    [fix] worker name, role and date

--- src/app/pages/worker/worker.component.ts
+++ src/app/pages/worker/worker.component.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { ShiftsTableComponent } from '../../shared/shifts-table/shifts-table.component'
 import { WorkerScheduleComponent } from '../../shared/worker-schedule/worker-schedule.component';
 import {NavbarMainComponent} from "../../shared/navbar-main/navbar-main.component";
+import {AuthService} from "../../core/auth.service";
+import {map} from "rxjs";
+import { Timestamp } from 'firebase/firestore';
+import {  DatePipe } from "@angular/common";
 
 @Component({
   selector: 'app-worker',
   standalone: true,
-  imports: [ShiftsTableComponent, WorkerScheduleComponent, NavbarMainComponent],
+  imports: [ShiftsTableComponent, WorkerScheduleComponent, NavbarMainComponent, DatePipe],
   templateUrl: './worker.component.html',
   styleUrls: ['./worker.component.css']
 })
-export class WorkerComponent {
+export class WorkerComponent implements OnInit{
+
+  worker: any;
+  timeData: any;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+        this.loadEmployeeData()
+    this.loadTimeData()
+    }
+
+  loadEmployeeData() {
+    this.authService.getUserData().pipe(
+        map(user => ({
+          name: user?.name ?? '',
+          role: user?.role ?? ''
+        }))
+    ).subscribe(data => {
+      this.worker = data;
+    });
+  }
+
+
+  loadTimeData() {
+    this.timeData = Timestamp.now().toDate();
+  }
+
+
+
+
+
 
 }
