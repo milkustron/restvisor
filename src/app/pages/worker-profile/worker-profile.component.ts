32f136c9b40999a90c05c433beef361e62b4cce2
Pedro <pedrobatistasosa2002@gmail.com>
2025-05-02 01:47:05 +0100

    [add] worker profile component

--- src/app/pages/worker-profile/worker-profile.component.ts
+++ src/app/pages/worker-profile/worker-profile.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {NavbarMainComponent} from "../../shared/navbar-main/navbar-main.component";
+import {ShiftsTableComponent} from "../../shared/shifts-table/shifts-table.component";
+import {WorkerScheduleComponent} from "../../shared/worker-schedule/worker-schedule.component";
 
 @Component({
   selector: 'app-worker-profile',
-  imports: [],
+  imports: [
+    NavbarMainComponent,
+    ShiftsTableComponent,
+    WorkerScheduleComponent
+  ],
   templateUrl: './worker-profile.component.html',
   styleUrl: './worker-profile.component.css'
 })
