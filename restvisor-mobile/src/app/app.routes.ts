22e775aa3c93513157f025f21794544adb344a8f
hupog <hugogonzalez798@gmail.com>
2025-05-12 16:12:04 +0100

    feat: update routing paths and component imports for authentication and favorites

--- restvisor-mobile/src/app/app.routes.ts
+++ restvisor-mobile/src/app/app.routes.ts
@@ -2,33 +2,32 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'folder/inbox',
+    path: '**',
+    redirectTo: 'detail',
     pathMatch: 'full',
-  },
-  {
-    path: 'folder/:id',
-    loadComponent: () =>
-      import('./folder/folder.page').then((m) => m.FolderPage),
   },
   {
     path: 'register',
     loadComponent: () =>
-      import('./auth/register/register.page').then((m) => m.RegisterPage),
+      import('./auth/register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
   },
   {
     path: 'login',
     loadComponent: () =>
-      import('./auth/login/login.page').then((m) => m.LoginPage),
+      import('./auth/login/login.component').then((m) => m.LoginComponent),
   },
   {
     path: 'favorites',
     loadComponent: () =>
-      import('./favorites/favorites.page').then((m) => m.FavoritesPage),
+      import('./favorites/favorites.component').then(
+        (m) => m.FavoritesComponent
+      ),
   },
   {
     path: 'detail',
     loadComponent: () =>
-      import('./detail/detail.page').then((m) => m.DetailPage),
+      import('./details/details.component').then((m) => m.DetailsComponent),
   },
 ];
