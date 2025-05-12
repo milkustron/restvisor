39064be1ec3d96fb50df5cb9b74150e30522f31b
hupog <hugogonzalez798@gmail.com>
2025-05-12 15:47:59 +0100

    chore: update dependencies and integrate Firebase services
    - Updated Angular and Ionic dependencies in package.json.
    - Added Firebase dependencies: @angular/fire and firebase.
    - Integrated Firebase initialization and services (Auth, Firestore) in main.ts.
    - Created a new firebase.json file.

--- restvisor-mobile/src/main.ts
+++ restvisor-mobile/src/main.ts
@@ -4,11 +4,14 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({ projectId: "restvisor-887b9", appId: "1:674854508849:web:dd6b8ca735af7f4ab07810", databaseURL: "https://restvisor-887b9-default-rtdb.europe-west1.firebasedatabase.app", storageBucket: "restvisor-887b9.firebasestorage.app", apiKey: "AIzaSyDHAtQI2VBlhvYI8Vtmy7Pk2LqBUy4O1cM", authDomain: "restvisor-887b9.firebaseapp.com", messagingSenderId: "674854508849", measurementId: "G-Z6GVHFKTXD" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ],
 });
