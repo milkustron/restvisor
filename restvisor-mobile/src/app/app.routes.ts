aebea811eed90027e2d56f7c6c81752dcf7114f1
hupog <hugogonzalez798@gmail.com>
2025-05-12 15:59:15 +0100

    feat: add authentication and detail pages with routing

--- restvisor-mobile/src/app/app.routes.ts
+++ restvisor-mobile/src/app/app.routes.ts
@@ -11,4 +11,24 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
   },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./auth/register/register.page').then((m) => m.RegisterPage),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./auth/login/login.page').then((m) => m.LoginPage),
+  },
+  {
+    path: 'favorites',
+    loadComponent: () =>
+      import('./favorites/favorites.page').then((m) => m.FavoritesPage),
+  },
+  {
+    path: 'detail',
+    loadComponent: () =>
+      import('./detail/detail.page').then((m) => m.DetailPage),
+  },
 ];
--- restvisor-mobile/src/main.ts
+++ restvisor-mobile/src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -12,6 +20,21 @@ bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({ projectId: "restvisor-887b9", appId: "1:674854508849:web:dd6b8ca735af7f4ab07810", databaseURL: "https://restvisor-887b9-default-rtdb.europe-west1.firebasedatabase.app", storageBucket: "restvisor-887b9.firebasestorage.app", apiKey: "AIzaSyDHAtQI2VBlhvYI8Vtmy7Pk2LqBUy4O1cM", authDomain: "restvisor-887b9.firebaseapp.com", messagingSenderId: "674854508849", measurementId: "G-Z6GVHFKTXD" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'restvisor-887b9',
+        appId: '1:674854508849:web:dd6b8ca735af7f4ab07810',
+        databaseURL:
+          'https://restvisor-887b9-default-rtdb.europe-west1.firebasedatabase.app',
+        storageBucket: 'restvisor-887b9.firebasestorage.app',
+        apiKey: 'AIzaSyDHAtQI2VBlhvYI8Vtmy7Pk2LqBUy4O1cM',
+        authDomain: 'restvisor-887b9.firebaseapp.com',
+        messagingSenderId: '674854508849',
+        measurementId: 'G-Z6GVHFKTXD',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 });
